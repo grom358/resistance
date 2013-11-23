d68dfd9956682e7687c2b70767cfadbae98c9044
Andrew <Andrew@Uzumaki>
2013-11-23 23:13:02 +1000

    Have handler communicating with GameManager + GameSession

---
+++ src/resistance/GameManager.java
@@ -0,0 +1,43 @@
+package resistance;
+
+import io.netty.channel.Channel;
+import java.util.Map;
+
+/**
+ *
+ * @author Andrew
+ */
+public class GameManager {
+    private static final GameManager SINGLETON = new GameManager();
+    private int sessionId = 0;
+    private GameSession gameSession;
+    private int playerIndex;
+    
+    public static GameManager getInstance(){
+        return SINGLETON;
+    }
+    private GameManager() {
+        startSession();
+    }
+
+    public ConnectionDetails getConnection(Channel channel) {
+        gameSession.players[playerIndex].channel = channel;
+        playerIndex++;
+        if (playerIndex == gameSession.players.length) {
+            new Thread(gameSession).start();
+            startSession();
+        }
+        return (new ConnectionDetails(gameSession, playerIndex));
+    }
+
+    private void startSession() {
+        PlayerSession[] playerSessions = new PlayerSession[5];
+        for(int i = 0; i < playerSessions.length; i++){
+            playerSessions[i] = new PlayerSession(i + "");
+        }
+        gameSession = new GameSession(sessionId, playerSessions);
+        sessionId++;
+        playerIndex = 0;
+    }
+
+}
